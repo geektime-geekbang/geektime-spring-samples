24af291bc7e211142d0596c44bade729f8a7e804
DigitalSonic <digitalsonic.dxf@gmail.com>
2019-01-07 00:46:17 +0800

    增加JdbcTemplate演示

--- Chapter 2/datasource-demo/src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
+++ Chapter 2/datasource-demo/src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 @Slf4j
@@ -15,16 +17,29 @@ public class DataSourceDemoApplication implements CommandLineRunner {
 	@Autowired
 	private DataSource dataSource;
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DataSourceDemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		showConnection();
+		showData();
+	}
+
+	private void showConnection() throws SQLException {
 		log.info(dataSource.toString());
 		Connection conn = dataSource.getConnection();
 		log.info(conn.toString());
 		conn.close();
+	}
+
+	private void showData() {
+		jdbcTemplate.queryForList("SELECT * FROM FOO")
+				.forEach(row -> log.info(row.toString()));
 	}
 }
 
